35dcbcb20d7b2d6f3193bda08e24d434ce6b6b05
Alu-Naofumi-J-Iwatani <joseabrahammaciastorres@gmail.com>
2021-03-24 16:39:48 +0000

    TuMascota v1.0.5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,25 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
-//const bodyParser = require('body-parser');
+
 
 const { renderIndex, renderLoguin, createPerfil } = require('../controlers/entries.controler');
-//const { request } = require('express');
-//Setting to entry dates
 
-//Create application/x-www-form-urlencoded parser
-//const urlencoded = bodyParser.urlencoded({ extended: true });
-
-
-
-
-//Routes
-//POST/loguin gets urlencoded bodies
-/*
-router.post('/loguin', bodyParser.urlencoded, (req, res) => {
-    res.send('Recived ' + bodyParser.json.toString + " and te request: => " + request.body.toString);
-})
-*/
 
 //-  /
 router.get('/', renderIndex);
@@ -28,6 +13,7 @@ router.get('/', renderIndex);
 //  /loguin
 router.get('/loguin', renderLoguin);
 
+//Entry Data
 router.post('/loguin', createPerfil);
 
 
